3305a96e47d44a2dd112b6ba1f30c51b6cb06d6b
virginiah894 <virgyperry@gmail.com>
2019-10-26 13:17:36 +0300

    add input on the author component

--- src/app/quoteauthor/quoteauthor.component.ts
+++ src/app/quoteauthor/quoteauthor.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Quote } from "../Quote"
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quoteauthor',
@@ -6,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quoteauthor.component.css']
 })
 export class QuoteauthorComponent implements OnInit {
-
+@Input() quote: Quote;
   constructor() { }
 
   ngOnInit() {
