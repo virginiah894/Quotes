d35564605746179521ad3f02c60c2276337173da
virginiah894 <virgyperry@gmail.com>
2019-10-26 12:36:41 +0300

    exports lofgic to quote component from root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,5 @@ import { quote } from "./Quote";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-quotes:Quote =[
-{id:1,name: "Be inspired before you expire"},
-{id:2,name :"We are what we believe we are!"},
-{id:3,name:"Change your thoughts and you change your world"},
-{id:4,name: "If opportunity doesn't knock, build a door"},
-];
+
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../Quote';
+
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes:Quote =[
+    {id:1,name: "Be inspired before you expire"},
+    {id:2,name :"We are what we believe we are!"},
+    {id:3,name:"Change your thoughts and you change your world"},
+    {id:4,name: "If opportunity doesn't knock, build a door"},
+    ];
 
   constructor() { }
 
