79bdd38a333d91f2287619b6e33b02b72958750b
virginiah894 <virgyperry@gmail.com>
2019-10-26 23:40:22 +0300

    add directive for highlighting quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteauthorComponent } from './quoteauthor/quoteauthor.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuoteauthorComponent
+    QuoteauthorComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
